2be047c7ba2f2e8741b0079e7aff3880daa10fa3
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-12-03 12:43:02 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+##################################################################
+#global variables#
+rocketMass = 5
+#                 z y x
+initWindVector = (4,2,6)
+windFieldSize = 100 # 100x100x100
+initRocketAngle = (1,0,0)
+windStdDev = 0
+heightScale = 0 # i.e. if heightScale = 2, stddev at top is twice stddev at bottom
+##################################################################
 
