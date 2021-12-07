1d8c8fa6b4af56b4f60d9016b5af1b06f81d64f2
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-12-06 19:05:39 -0800

    Update wind.py

--- wind.py
+++ wind.py
@@ -3,16 +3,6 @@ import numpy as np
 import math
 
 ##############################################################################
-# user defined variables #
-rocketMass = 5
-#                 z y x
-initWindVector = (4,2,6)
-windFieldSize = 100 # 100x100x100
-initRocketAngle = (1,0,0)
-windStdDev = 1.0
-heightStdDevScale = 2 # i.e. if heightStdDevScale = 2, stddev at top level is twice stddev at bottom level
-heightMagScale = 2 # i.e if heightMagScale = 2, magnitude of vectors at top level is twice the magnitude of vectors at bottom level
-##############################################################################
 # consts #
 DIMS = 3
 ##############################################################################
@@ -57,6 +47,13 @@ def applyStdDev(wind, windStdDev, heightStdDevScale, windFieldSize, DIMS):
         wind[height,:,:,:] += np.random.uniform(low=-((windStdDev + (stdDevStep * height)) * 2),
                                                 high=((windStdDev + (stdDevStep * height)) * 2),
                                                 size=(windFieldSize, windFieldSize, DIMS))
+    return wind
+    
+    
+def createWind(initWindVector, windFieldSize, heightMagScale, windStdDev, heightStdDevScale):
+    wind = createWindField(initWindVector, windFieldSize, DIMS)
+    wind = applyMagScale(wind, heightMagScale, windFieldSize)
+    wind = applyStdDev(wind, windStdDev, heightStdDevScale, windFieldSize, DIMS)
     return wind
     
 
