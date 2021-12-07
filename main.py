6332eefc026dfd7781c85213abe2c5ef499ff4dc
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-12-06 19:04:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import rocket as R
-##################################################################
-#global variables#
+from wind import *  
+##############################################################################
+# user defined variables #
 rocketMass = 5
 #                 z y x
 initWindVector = (4,2,6)
 windFieldSize = 100 # 100x100x100
 initRocketAngle = (1,0,0)
-windStdDev = 0
-heightScale = 0 # i.e. if heightScale = 2, stddev at top is twice stddev at bottom
+windStdDev = 1.0
 heightStdDevScale = 2 # i.e. if heightStdDevScale = 2, stddev at top level is twice stddev at bottom level
 heightMagScale = 2 # i.e if heightMagScale = 2, magnitude of vectors at top level is twice the magnitude of vectors at bottom level
-##################################################################
+##############################################################################
 
-
+if __name__ == '__main__':
+  wind = createWind(initWindVector, windFieldSize, heightMagScale, windStdDev, heightStdDevScale)
