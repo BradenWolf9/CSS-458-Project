3c02dc115a2ff5cf6b426e487494be01589d12e5
BradenWolf9 <72713649+BradenWolf9@users.noreply.github.com>
2021-12-07 21:07:24 -0800

    Update wind.py

--- wind.py
+++ wind.py
@@ -11,6 +11,8 @@ DIMS = 3
 # creates wind field with each 3D cell set to the given wind vector
 # pre: user defined variables are set
 # post: 3D wind field is created with only initial wind vector in each cell
+# return: 4D array. First element is height, second is north south, third is
+#         east west, fourth is 3D vector of height, north south, east west
 def createWindField(initWindVector, windFieldSize, DIMS):
     wind = np.zeros((windFieldSize, windFieldSize, windFieldSize, DIMS))
     wind[:,:,:,:] = initWindVector[:]
@@ -21,6 +23,8 @@ def createWindField(initWindVector, windFieldSize, DIMS):
 # pre: 3D wind field is created with only initial wind vector in each cell
 # post: 3D wind is altered so magnitude of vectors scale with height according
 #       to given magnitude scaling
+# return: 4D array. First element is height, second is north south, third is
+#         east west, fourth is 3D vector of height, north south, east west
 def applyMagScale(wind, heightMagScale, windFieldSize):
     magStep = (heightMagScale - 1) / (windFieldSize - 1)
     # distance formula
@@ -40,6 +44,8 @@ def applyMagScale(wind, heightMagScale, windFieldSize):
 # post: 3D wind field is altered to be random according to given standard
 #       deviation and standard deviation is scaled with height according to 
 #       the given standard deviation scaling
+# return: 4D array. First element is height, second is north south, third is
+#         east west, fourth is 3D vector of height, north south, east west
 def applyStdDev(wind, windStdDev, heightStdDevScale, windFieldSize, DIMS):
     stdDevStep = ((windStdDev * heightStdDevScale) - windStdDev) / windFieldSize
     # for each height level
@@ -50,6 +56,10 @@ def applyStdDev(wind, windStdDev, heightStdDevScale, windFieldSize, DIMS):
     return wind
     
     
+# Combines the creating wind functions into one, so can call one function
+# to create wind.
+# return: 4D array. First element is height, second is north south, third is
+#         east west, fourth is 3D vector of height, north south, east west    
 def createWind(initWindVector, windFieldSize, heightMagScale, windStdDev, heightStdDevScale):
     wind = createWindField(initWindVector, windFieldSize, DIMS)
     wind = applyMagScale(wind, heightMagScale, windFieldSize)
