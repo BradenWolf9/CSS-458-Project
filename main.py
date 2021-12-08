9e558cd5bc8e9227fce7562ccd0d6ad46f9786d7
Tim Kozlov <timkozlov@gmail.com>
2021-12-08 14:11:40 -0800

    Added more descriptive comments to variables

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import tests as T
 import numpy as N
 
 ##############################################################################
-# user defined variables #
-rocketMass = 5
-#                 z y x
-initWindVector = (0,0,0)
-#                 z y x   z: height, y: north(+) south(-), x: east(+) west(-)
-#initWindVector = (4,2,6)
-windFieldSize = 500 # windFieldSize * (1x1x1)
-initRocketAngle = (1,0,0)
-windStdDev = 0.0
-heightStdDevScale = 1 # i.e. if heightStdDevScale = 2, stddev at top level is twice stddev at bottom level
-heightMagScale = 1 # i.e if heightMagScale = 2, magnitude of vectors at top level is twice the magnitude of vectors at bottom level
-timeSlice = .05
-initialPosition = N.array([0,windFieldSize/2,windFieldSize/2])
+initWindVector = (0,0,0) # Initial wind velocity (up, north, east), in meters/sec
+windFieldSize = 200 # Length of one side of wind field, in meters
+windStdDev = 0 # Standard deviation of initial wind velocity (meters/sec)
+heightStdDevScale = 2 # i.e. if heightStdDevScale = 2, stddev at top level is twice stddev at bottom level
+heightMagScale = 2 # i.e if heightMagScale = 2, magnitude of vectors at top level is twice the magnitude of vectors at bottom level
+
+initRocketAngle = (1.0,0.0,0.0)  # Rocket direction unit vector (up, north, east)
+initialPosition = N.array([0,windFieldSize/2,windFieldSize/2]) # Rocket position, in meters
+
+timeSlice = .05 # Simulation time slice, in seconds
+
 tests = True # valid entries are True (runs tests) or False
 ##############################################################################
 
 if __name__ == '__main__':
-  wind = W.createWind(initWindVector, windFieldSize, heightMagScale, windStdDev, heightStdDevScale)
-  positions = R.launchRocket(initRocketAngle, wind, timeSlice, initialPosition)
-  if tests:
-    T.execute()
+    # Generate a wind vector with initial velocity + randomness applied
+    wind = W.createWind(initWindVector, windFieldSize, heightMagScale, windStdDev, heightStdDevScale)
+    # Run the simulation and calculate positions
+    positions = R.launchRocket(initRocketAngle, wind, timeSlice, initialPosition)
+    # Execute tests if enabled
+    if tests:
+        T.execute()
   
