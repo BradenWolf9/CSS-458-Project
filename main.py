865617eae0ac9e8a58e9f48db9e1c3fa4c62bc50
Tim Kozlov <timkozlov@gmail.com>
2021-12-08 16:26:44 -0800

    Playing with some values

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import numpy as N
 import visualize
 
 ##############################################################################
-initWindVector = (0,-5,0) # Initial wind velocity (up, north, east), in meters/sec
+initWindVector = (0,0,0) # Initial wind velocity (up, north, east), in meters/sec
 windFieldSize = 300 # Length of one side of wind field, in meters
-windStdDev = 2 # Standard deviation of initial wind velocity (meters/sec)
-heightStdDevScale = 2 # i.e. if heightStdDevScale = 2, stddev at top level is twice stddev at bottom level
+windStdDev = 3 # Standard deviation of initial wind velocity (meters/sec)
+heightStdDevScale = 3 # i.e. if heightStdDevScale = 2, stddev at top level is twice stddev at bottom level
 heightMagScale = 2 # i.e if heightMagScale = 2, magnitude of vectors at top level is twice the magnitude of vectors at bottom level
 
-initRocketAngle = (0.8,0.3,0.0)  # Rocket direction unit vector (up, north, east)
+initRocketAngle = (1.0,0.0,0.0)  # Rocket direction unit vector (up, north, east)
 initialPosition = N.array([0,windFieldSize/2,windFieldSize/2]) # Rocket position, in meters
 
 timeSlice = .05 # Simulation time slice, in seconds
-numSimulations = 200
+numSimulations = 500
 tests = True # valid entries are True (runs tests) or False
 ##############################################################################
 
